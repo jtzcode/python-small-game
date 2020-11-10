ce64f224a561afbccc21a02ac44660324489e8d5
Tianze Jiang <tianze.jiang@citrix.com>
2020-11-10 16:03:34 +0800

    Start the small game

--- game.py
+++ game.py
@@ -1,17 +1,22 @@
 import random
 import textwrap
 
-if __name__ == '__main__':
-    keep_playing = 'y'
-    occupants = ['enemy', 'friend', 'unocuppied']
-    width = 72
-    dotted_line = '_' * width
-    print(dotted_line)
-    print("\033[1m" + "Attacks of the Orcs v1.0: " + "\033[0m")
+def print_bold(msg, end='\n'):
+    """Print a string in 'bold' font"""
+    print("\033[1m" + msg + "\033[0m", end=end)
 
+
+def print_dotted_line(width=72):
+    """Print a dotted (rather 'dashed') line"""
+    print('-'*width)
+
+def show_theme_message(width):
+    """Print the game theme in the terminal window"""
+    print_dotted_line()
+    print_bold("Attack of The Orcs v0.0.5:")
     msg = (
         "The war between humans and their arch enemies, Orcs, was in the "
-        "offing. Sir Foo, one of the brave knights guarding the southern "
+        "offing. Sir Eggy, one of the brave knights guarding the southern "
         "plains began a long journey towards the east through an unknown "
         "dense forest. On his way, he spotted a small isolated settlement."
         " Tired and hoping to replenish his food stock, he decided to take"
@@ -19,10 +24,67 @@ if __name__ == '__main__':
         "was no one to be seen around. Hesitantly, he  decided to enter..")
 
     print(textwrap.fill(msg, width=width))
-    print("\033[1m" + "Mission:" + "\033[0m")
-    print("\tChoose a hut where Sir Foo can rest...")
-    print("\033[1m" + "TIP:" + "\033[0m")
-    print("Be careful as there are enemies lurking around!")
-    print(dotted_line)
 
-    
+
+def show_game_mission():
+    """Print the game mission in the terminal window"""
+    print_bold("Mission:")
+    print("\tChoose a hut where Sir Foo can rest...")
+    print_bold("TIP:")
+    print("Be careful as there are enemies lurking around!")
+    print_dotted_line()
+
+
+def reveal_occupants(idx, huts):
+    """Print the occupants of the hut"""
+    msg = ""
+    print("Revealing the occupants...")
+    for i in range(len(huts)):
+        occupant_info = "<%d:%s>" % (i+1, huts[i])
+        if i + 1 == idx:
+            occupant_info = "\033[1m" + occupant_info + "\033[0m"
+        msg += occupant_info + " "
+
+    print("\t" + msg)
+    print_dotted_line()
+
+def occupy_huts():
+    """Randomly populate the `huts` list with occupants"""
+    huts = []
+    occupants = ['enemy', 'friend', 'unoccupied']
+    while len(huts) < 5:
+        computer_choice = random.choice(occupants)
+        huts.append(computer_choice)
+    return huts
+
+
+def process_user_choice():
+    """Accepts the hut number from the user"""
+    msg = "\033[1m" + "Choose a hut number to enter (1-5): " + "\033[0m"
+    user_choice = input("\n" + msg)
+    idx = int(user_choice)
+    return idx
+
+def play_game(idx, huts):
+    print("\033[1m" + "Entering hut %d... " % idx + "\033[0m", end=' ')
+    if huts[idx - 1] != 'enemy':
+        print_bold("Congratulations! YOU WIN!!!")
+    else:
+        print_bold("YOU LOSE  :(  Better luck next time")
+
+def run_application():
+    keep_playing = 'y'
+    width = 72
+
+    show_theme_message(width)
+    show_game_mission()
+
+    while keep_playing == 'y':
+        huts = occupy_huts()
+        index = process_user_choice()
+        reveal_occupants(index, huts)
+        play_game(index, huts)
+        keep_playing = input("Play again? Yes(y)/No(n):")
+
+if __name__ == '__main__':
+    run_application()
